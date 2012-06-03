28ab32aa1d670df93dfb1544a4697cfaa0a71944
gzb1985 <gzb1985@gmail.com>
2012-06-03 18:45:32 +0800

    使用GeoModel库实现GAE上的地理位置数据查询
    GeoModel地址（http://code.google.com/p/geomodel/source/browse/trunk/）

--- examples/gae-google-maps-example/index.py
+++ examples/gae-google-maps-example/index.py
@@ -4,6 +4,8 @@ from bottle import static_file, response, template, request
 from bottle import jinja2_view as view
 from google.appengine.ext import db
 
+from geo import geotypes
+
 import item
 from item import Item
 item.insert_def_items()
@@ -16,18 +18,63 @@ def main_page():
 @route('/item/latest')
 def latest_items():
     response.content_type = 'application/json'
-    items = db.GqlQuery( "SELECT * FROM Item").fetch(5)
-    result_obj = []
+    items = db.GqlQuery( "SELECT * FROM Item").fetch(10)
+    return results_dump(items)
+
+
+#http://localhost:8082/item/nearby?type=center&lat=31.199&lon=121.587&maxresults=10&maxdistance=10000
+#http://localhost:8082/item/nearby?type=box&north=33.297&east=122.687&south=30.000&west=121.200&maxresults=100
+@route('/item/nearby')
+def nearby_items():
+    response.content_type = 'application/json'
+    
+    if (nearby_param_validate(request) == False):
+        return {'status':'error-query-paras'}
+    
+    try:
+        if (request.query.type == 'box'):
+            bounds = geotypes.Box(float(request.query.north), float(request.query.east),
+                                  float(request.query.south), float(request.query.west))
+        elif (request.query.type == 'center'):
+            center = geotypes.Point(float(request.query.lat), float(request.query.lon))
+    except:
+        return {'status':'invalid-query-paras'}
+    
+    max_results = int(request.query.maxresults or '100')
+    max_distance = float(request.query.maxdistance or '80000')  # 80 km ~ 50 mi
+    
+    try:
+        base_query = Item.all()
+        if (request.query.type == 'box'):
+            results = Item.bounding_box_fetch(base_query, bounds, max_results = max_results)
+        elif (request.query.type == 'center'):
+            results = Item.proximity_fetch(base_query, center, max_results = max_results, max_distance=max_distance)
+        return results_dump(results)
+    except:
+      return {'status':'error-database-query'}
+
+def results_dump(items):
     if len(items) == 0 :
-        return {'status':'error'}
-    else:
-        for item in items :
-            item_dict = {}
-            item_dict['title'] = item.title
-            item_dict['lat'] = item.location.lat
-            item_dict['lon'] = item.location.lon
-            result_obj.append(item_dict)
-    return {'status':'success', 'results' : result_obj}
+        return {'status':'non-results'}
+    
+    result_obj = []
+    for item in items :
+        item_dict = {}
+        item_dict['title'] = item.title
+        item_dict['lat'] = item.location.lat
+        item_dict['lon'] = item.location.lon
+        result_obj.append(item_dict)
+    return {'status':'success', 'num': len(items), 'results' : result_obj}
+
+def nearby_param_validate(request):
+    if (request.query.type):
+        if (request.query.type == 'box'):
+            if (request.query.north and request.query.east and request.query.south and request.query.west):
+                return True
+        elif (request.query.type == 'center'):
+            if (request.query.lat and request.query.lon):
+                return True
+    return False
 
 
 @route('/static/css/:filename')
@@ -41,6 +88,8 @@ def server_static(filename):
 @route('/static/:filename')
 def server_static(filename):
     return static_file(filename, root='static')
+
+
 
 
 bottle.debug(True)
--- examples/gae-google-maps-example/item.py
+++ examples/gae-google-maps-example/item.py
@@ -1,20 +1,73 @@
 from google.appengine.ext import db
+from geo.geomodel import GeoModel
 
-class Item(db.Model):
+class Item(GeoModel):
   title = db.StringProperty()
   user = db.StringProperty()
   price = db.StringProperty()
   desc = db.StringProperty()
-  location = db.GeoPtProperty()
+  
+  @staticmethod
+  def public_attributes():
+    """Returns a set of simple attributes on public school entities."""
+    return [
+      'title', 'user', 'price', 'desc', 'state'
+      ]
+
+  def _get_latitude(self):
+    return self.location.lat if self.location else None
+
+  def _set_latitude(self, lat):
+    if not self.location:
+      self.location = db.GeoPt()
+    self.location.lat = lat
+
+  latitude = property(_get_latitude, _set_latitude)
+
+  def _get_longitude(self):
+    return self.location.lon if self.location else None
+
+  def _set_longitude(self, lon):
+    if not self.location:
+      self.location = db.GeoPt()
+    self.location.lon = lon
+
+  longitude = property(_get_longitude, _set_longitude)
 
 def insert_def_items():
     items = db.GqlQuery( "SELECT * FROM Item").fetch(1)
     if len(items) == 0 :
         itemarray= []
-        itemarray.append(Item( title = 'iphone1', user = 'jimmy', price = '1000', desc = 'sixty percent new', location = db.GeoPt(31.199, 121.587)))
-        itemarray.append(Item( title = 'iphone2', user = 'bob', price = '1500', desc = 'sixty-five percent new', location = db.GeoPt(31.297, 121.587)))
-        itemarray.append(Item( title = 'iphone3', user = 'john', price = '2000', desc = 'seventy percent new', location = db.GeoPt(31.096, 121.587)))
-        itemarray.append(Item( title = 'iphone4', user = 'spiderman', price = '3000', desc = 'eigty percent new', location = db.GeoPt(31.199, 121.487)))
-        itemarray.append(Item( title = 'iphone5', user = 'ironman', price = '5000', desc = 'ninty percent new', location = db.GeoPt(31.297, 121.687)))
-        itemarray.append(Item( title = 'iphone6', user = 'superman', price = '10000', desc = 'comming soon', location = db.GeoPt(31.096, 121.787)))
+        item1 = Item( title = 'iphone1', user = 'jimmy', price = '1000', desc = 'sixty percent new', location = db.GeoPt(31.199, 121.587))
+        item1.update_location()
+        itemarray.append(item1)
+
+        item2 = Item( title = 'iphone2', user = 'bob', price = '1500', desc = 'sixty-five percent new', location = db.GeoPt(31.297, 121.587))
+        item2.update_location()
+        itemarray.append(item2)
+
+        item3 = Item( title = 'iphone3', user = 'john', price = '2000', desc = 'seventy percent new', location = db.GeoPt(31.096, 121.587))
+        item3.update_location()
+        itemarray.append(item3)
+
+        item4 = Item( title = 'iphone4', user = 'spiderman', price = '3000', desc = 'eigty percent new', location = db.GeoPt(31.199, 121.487))
+        item4.update_location()
+        itemarray.append(item4)
+
+        item5 = Item( title = 'iphone5', user = 'ironman', price = '5000', desc = 'ninty percent new', location = db.GeoPt(31.297, 121.687))
+        item5.update_location()
+        itemarray.append(item5)
+
+        item6 = Item( title = 'iphone6', user = 'superman', price = '10000', desc = 'comming soon', location = db.GeoPt(31.096, 121.787))
+        item6.update_location()
+        itemarray.append(item6)
+
+        item7 = Item( title = 'iphone7', user = 'loki', price = '100000', desc = 'comming soon', location = db.GeoPt(30.096, 121.287))
+        item7.update_location()
+        itemarray.append(item7)
+
+        item8 = Item( title = 'iphone8', user = 'thor', price = '1000000', desc = 'comming soon', location = db.GeoPt(32.096, 121.287))
+        item8.update_location()
+        itemarray.append(item8)
+        
         db.put(itemarray)
